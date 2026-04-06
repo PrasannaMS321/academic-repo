f8b246b4f33b10709dbddad2ba6d2a84c44cf76b
Prasanna MS <107895759+PrasannaMS321@users.noreply.github.com>
2026-04-06 10:02:18 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -240,6 +240,8 @@ app.use((err, req, res, next) => {
     }
 })
 
-app.listen(5000, () => {
-    console.log("🚀 Server running on http://localhost:5000");
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+    console.log(`🚀 Server running on port ${PORT}`);
 });
